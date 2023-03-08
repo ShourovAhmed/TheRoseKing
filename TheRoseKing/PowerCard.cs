002f3ba79c6d103f2b2a2000cdda0a1602d99d03
Shourov Ahmed <shourov.ahmed@student.ap.be>
2023-03-09 00:02:05 +0100

    Add method createPowerCards

--- TheRoseKing/PowerCard.cs
+++ TheRoseKing/PowerCard.cs
@@ -6,7 +6,38 @@ using System.Threading.Tasks;
 
 namespace TheRoseKing
 {
+    public enum Direction { Left, Right, Up, Down };
     internal class PowerCard
     {
+        public static List<PowerCard> powerCards= new List<PowerCard>();
+
+        int distance;
+        Direction direction;
+
+        static Random r = new Random();
+        
+
+        public PowerCard(Direction direction, int distance) {
+
+            this.distance = distance;
+            this.direction = direction;
+        
+        }
+
+        public static void createPowerCards()
+        {
+            for (int i = 0; i < 24; i++)
+            {
+                int dist = r.Next(1,4);
+                Direction d = (Direction) r.Next(0, 4);
+
+                //Console.WriteLine($"{d}, {dist}");
+
+                PowerCard p = new PowerCard(d, dist);
+                powerCards.Add(p);
+            }
+        }
+
+
     }
 }
